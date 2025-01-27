09b85e7a9c23998f2e8c27216db25fa9002dbaac
Ilya Andriyanov <AndriyanovIM@kamaz.ru>
2025-01-27 15:29:21 +0300

    feature: halfway urdf files

---
+++ src/agrobot_urdf/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'agrobot_urdf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aim',
+    maintainer_email='aim@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
